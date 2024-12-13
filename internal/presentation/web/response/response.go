eed8d7876f8245c159b1e9e5089d45deb6e7ba18
zmskv <zmskvxd@gmail.com>
2024-12-13 22:44:50 +0300

    Initial commit

---
+++ internal/presentation/web/response/response.go
@@ -0,0 +1,19 @@
+package response
+
+import "github.com/gin-gonic/gin"
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+type SuccessResponse struct {
+	Message string `json:"message"`
+}
+
+func NewErrorResponse(c *gin.Context, statusCode int, message string) {
+	c.AbortWithStatusJSON(statusCode, ErrorResponse{message})
+}
+
+func NewSuccessResponse(c *gin.Context, statusCode int, message string) {
+	c.AbortWithStatusJSON(statusCode, SuccessResponse{message})
+}
