b142378ce8a6a0e28848ba34838c6a72c3958d0f
paintingpromisesss <paintingpromisesss@gmail.com>
2025-04-20 15:42:13 +0300

    added GetUserById GRPC method

--- internal/presentation/grpc/handlers/user.go
+++ internal/presentation/grpc/handlers/user.go
@@ -63,3 +63,29 @@ func (h *GRPCHandler) UpdateUserPassword(ctx context.Context, req *pb.UpdateUser
 
 	return &pb.UpdateUserPasswordResponse{Status: "success"}, nil
 }
+
+func (h *GRPCHandler) GetUserById(ctx context.Context, req *pb.GetUserByIdRequest) (*pb.GetUserByIdResponse, error) {
+	user, err := h.userService.GetById(ctx, req.Id)
+	if err != nil {
+		switch err {
+		case service.ErrUserNotFound:
+			return nil, status.Error(codes.NotFound, err.Error())
+		case service.ErrInvalidUUIDFormat:
+			return nil, status.Error(codes.InvalidArgument, err.Error())
+		default:
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+	}
+
+	return &pb.GetUserByIdResponse{
+		Id:        user.ID,
+		Username:  user.Username,
+		Email:     user.Email,
+		Password:  user.Password,
+		Role:      int32(user.Role),
+		Rating:    int32(user.Rating),
+		CreatedAt: timestamppb.New(user.CreatedAt),
+		UpdatedAt: timestamppb.New(user.UpdatedAt),
+		IsActive:  user.IsActive,
+	}, nil
+}
