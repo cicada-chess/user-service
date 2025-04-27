1981a256a68694706c82b6d62fbe5307d4148389
paintingpromisesss <paintingpromisesss@gmail.com>
2025-04-27 20:46:44 +0300

    fix

--- migrations/000001_create_profiles_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS profiles (
-    user_id UUID NOT NULL UNIQUE REFERENCES users(id) ON DELETE CASCADE PRIMARY KEY,
-    description TEXT,
-    age INT,
-    location VARCHAR(255),
-    avatar_path VARCHAR(512),
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-);
