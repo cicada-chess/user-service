70387ae42c53e23913803736cff4d6b5eb08e3cb
hope1ess <paintingpromisesss@gmail.com>
2025-05-03 00:50:30 +0300

    added patch to cors

--- internal/presentation/http/ginapp/routes.go
+++ internal/presentation/http/ginapp/routes.go
@@ -22,7 +22,7 @@ func InitRoutes(
 
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"}, // Разрешенные источники
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
 	}))
